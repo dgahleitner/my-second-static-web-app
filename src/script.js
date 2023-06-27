528bede0c194c24fc344a31a5895ff1a9293c22f
dgahleitner <david.gahleitner@gmx.net>
2023-06-27 16:35:47 +0200

    punkte hinzu + slider

--- src/script.js
+++ src/script.js
@@ -1,44 +1,88 @@
 let editing = false;
-let activePunkt = null; 
+let activePunkt = null;
+let punktCount = Number(localStorage.getItem('punktCount') || '0');
+let punktDiameterRatio = Number(localStorage.getItem('punktDiameterRatio') || '0.01');
+let punktDiameterPx;
+
 const tooltip = document.createElement('div');
 tooltip.classList.add('tooltip');
 document.body.appendChild(tooltip);
 
-document.getElementById('edit-btn').addEventListener('click', () => {
-  editing = !editing;
-  document.getElementById('edit-btn').innerText = editing ? 'Fertig' : 'Bearbeiten';
+function updatePunktSize() {
+  punktDiameterPx = document.getElementById('map').offsetWidth * punktDiameterRatio;
 
-  if (!editing) {
-    for (let punkt of punkte) {
-      localStorage.setItem(punkt.id, JSON.stringify({ left: punkt.style.left, top: punkt.style.top }));
-    }
+  const punkte = document.getElementsByClassName('punkt');
+  for (let punkt of punkte) {
+    punkt.style.width = punktDiameterPx + 'px';
+    punkt.style.height = punktDiameterPx + 'px';
   }
+}
+
+document.getElementById('punkt-slider').value = punktDiameterRatio * 1000;
+document.getElementById('punkt-slider').addEventListener('input', function() {
+  punktDiameterRatio = this.value / 1000;
+  localStorage.setItem('punktDiameterRatio', punktDiameterRatio.toString());
+  updatePunktSize();
 });
 
-const punkte = document.getElementsByClassName('punkt');
-for (let punkt of punkte) {
+function toggleEditMode() {
+  editing = !editing;
+  document.getElementById('edit-btn').innerText = editing ? 'Fertig' : 'Bearbeiten';
+  document.getElementById('add-btn').style.display = editing ? 'block' : 'none';
+  document.getElementById('delete-btn').style.display = editing ? 'block' : 'none';
+  document.getElementById('slider-div').style.display = editing ? 'block' : 'none';
+
+  if (!editing) {
+    const punkte = document.getElementsByClassName('punkt');
+    for (let punkt of punkte) {
+      let relativeLeft = punkt.offsetLeft / document.getElementById('map').offsetWidth;
+      let relativeTop = punkt.offsetTop / document.getElementById('map').offsetHeight;
+      localStorage.setItem(punkt.id, JSON.stringify({ left: relativeLeft, top: relativeTop }));
+    }
+    localStorage.setItem('punktCount', punktCount.toString());
+  }
+}
+
+function createPunkt() {
+  const punkt = document.createElement('div');
+  punkt.classList.add('punkt');
+  punkt.id = 'punkt' + (punktCount + 1);
+  document.getElementById('map').appendChild(punkt);
+  punkt.style.left = '50%';
+  punkt.style.top = '50%';
+  punktCount++;
+  initPunkt(punkt);
+}
+
+function deletePunkt() {
+  if (punktCount > 0) {
+    const punkt = document.getElementById('punkt' + punktCount);
+    document.getElementById('map').removeChild(punkt);
+    localStorage.removeItem(punkt.id);
+    punktCount--;
+  }
+}
+
+function initPunkt(punkt) {
   let offset = { x: 0, y: 0 };
   let isDown = false;
 
   const savedPosition = JSON.parse(localStorage.getItem(punkt.id));
   if (savedPosition) {
-    punkt.style.left = savedPosition.left;
-    punkt.style.top = savedPosition.top;
+    punkt.style.left = savedPosition.left * 100 + '%';
+    punkt.style.top = savedPosition.top * 100 + '%';
   }
 
-  // Generiere einen zufälligen Dezibelwert und setze die Farbe entsprechend
   let dB = Math.floor(Math.random() * 101);
-  let hue = 120 - (dB * 1.2); 
+  let hue = 120 - (dB * 1.2);
   punkt.style.backgroundColor = `hsl(${hue}, 100%, 50%)`;
 
   setInterval(() => {
-    // Generiere einen neuen Dezibelwert, der maximal ±10 vom aktuellen Wert abweicht
-    let change = Math.floor(Math.random() * 21) - 10; 
-    dB = Math.max(0, Math.min(100, dB + change)); 
+    let change = Math.floor(Math.random() * 21) - 10;
+    dB = Math.max(0, Math.min(100, dB + change));
     hue = 120 - (dB * 1.2);
     punkt.style.backgroundColor = `hsl(${hue}, 100%, 50%)`;
 
-    // Aktualisiere Tooltip-Text, wenn Tooltip für diesen Punkt angezeigt wird
     if (activePunkt === punkt) {
       tooltip.innerText = `${dB} dB`;
     }
@@ -81,8 +125,24 @@ for (let punkt of punkte) {
 
     event.preventDefault();
     if (isDown) {
-      punkt.style.left = (event.clientX + offset.x) + 'px';
-      punkt.style.top = (event.clientY + offset.y) + 'px';
+      punkt.style.left = ((event.clientX + offset.x) / document.getElementById('map').offsetWidth) * 100 + '%';
+      punkt.style.top = ((event.clientY + offset.y) / document.getElementById('map').offsetHeight) * 100 + '%';
     }
   }, true);
 }
+
+window.addEventListener('resize', updatePunktSize);
+
+document.getElementById('edit-btn').addEventListener('click', toggleEditMode);
+document.getElementById('add-btn').addEventListener('click', createPunkt);
+document.getElementById('delete-btn').addEventListener('click', deletePunkt);
+
+for (let i = 0; i < punktCount; i++) {
+  const punkt = document.createElement('div');
+  punkt.classList.add('punkt');
+  punkt.id = 'punkt' + (i + 1);
+  document.getElementById('map').appendChild(punkt);
+  initPunkt(punkt);
+}
+
+updatePunktSize();
